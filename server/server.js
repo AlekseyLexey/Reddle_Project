ea84475f8492765ca9cd0daacbee39d926479d79
EmpireStatePavlik <zzzlatan9@mail.ru>
2025-04-12 14:59:19 +0300

    server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,18 @@
-// СЕРВЕР ФАЙЛ!
+const express = require('express');
+const app = express();
+const indexRoute = require('./routes/indexRoutes')
+
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+app.use('/api', indexRoute)
+
+
+
+
+
+
+
+
+app.listen(3000, () => { console.log('Server started');
+});
