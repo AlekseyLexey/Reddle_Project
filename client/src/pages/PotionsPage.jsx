50928d900ac543ee76b15f920931266138a980fb
AlekseyAleksandrov <axcerz@gmail.com>
2025-04-12 18:44:54 +0300

    fix: bug with Link RRD

--- client/src/pages/PotionsPage.jsx
+++ client/src/pages/PotionsPage.jsx
@@ -1,25 +1,7 @@
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import fetchingData from "../services/api";
-
-function PotionsPage() {
-  const [potions, setPotions] = useState([]);
-
-  useEffect(() => {
-    fetchingData("/api/secrets").then(setPotions);
-  }, []);
-
-  return (
-    <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-      {potions.map((el, index) => {
-        return (
-          <Link to={`/riddle/${index + 1}`} key={index}>
-=======
 import secrets from "../../../secrets.json";
 import { useState } from "react";
-import './potionsPage.css'
-
+import { Link } from "react-router-dom";
+import "./potionsPage.css";
 
 function PotionsPage() {
   const [potions, setPotions] = useState([
@@ -34,19 +16,14 @@ function PotionsPage() {
       <div className="head">Выберите напиток </div>
       {potions.map((el, index) => {
         return (
-          <div key={index}>
->>>>>>> feat/StylePotionsPage
+          <Link to={`/riddle/${index + 1}`} key={index}>
             <img
               src={`potion-images/${el.image}`}
               alt="potion"
               style={{ width: "250px", height: "250px", objectFit: "cover" }}
             />
             <div>{el.author}</div>
-<<<<<<< HEAD
           </Link>
-=======
-          </div>
->>>>>>> feat/StylePotionsPage
         );
       })}
     </div>
