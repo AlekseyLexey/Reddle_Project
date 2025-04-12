d613a8577369506c49540ef3505b283802f3b7cd
Igor Isupov <igorisupov@Igors-MacBook-Air.local>
2025-04-12 21:45:49 +0700

    StylePositionsPage completed

--- client/src/App/App.jsx
+++ client/src/App/App.jsx
@@ -15,7 +15,7 @@ function App() {
       }}
     >
       <PotionsPage />
-      <QuestionsPotionPage />
+      {/* <QuestionsPotionPage /> */}
     </div>
   );
 }
--- client/src/pages/PotionsPage.jsx
+++ client/src/pages/PotionsPage.jsx
@@ -1,27 +1,33 @@
-import secrets from '../../../secrets.json';
-import { useState } from 'react';
+import secrets from "../../../secrets.json";
+import { useState } from "react";
+import './potionsPage.css'
+
 
 function PotionsPage() {
-  const [potions, setPotions] = useState([...secrets.simple_riddles, ...secrets.medium_riddles, ...secrets.hard_riddles])
-  console.log(potions)
+  const [potions, setPotions] = useState([
+    ...secrets.simple_riddles,
+    ...secrets.medium_riddles,
+    ...secrets.hard_riddles
+  ]);
+  console.log(potions);
 
-  return ( <div style={{display:'flex', flexWrap:'wrap', gap:'20px'}}>
-    {potions.map((el,index)=>{
-    return (
-      <div key={index}>
-        <img src={`potion-images/${el.image}`} alt='potion' style={{width:'250px', height:'250px', objectFit:'cover'}} />
-        <div>{el.author}</div>
-      </div>
-      
-    )
-  })}</div>
-)
-
-
-
-
-
-
+  return (
+    <div className="potions-container">
+      <div className="head">Выберите напиток </div>
+      {potions.map((el, index) => {
+        return (
+          <div key={index}>
+            <img
+              src={`potion-images/${el.image}`}
+              alt="potion"
+              style={{ width: "250px", height: "250px", objectFit: "cover" }}
+            />
+            <div>{el.author}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default PotionsPage
+export default PotionsPage;
